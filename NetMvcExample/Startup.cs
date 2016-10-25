e3da72df606955279b2bcdc2e76b161fbbb9225a
mchlek.b@gmail.com <mchlek.b@gmail.com>
2016-10-25 11:14:06 +0200

    init

---
+++ NetMvcExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetMvcExample.Startup))]
+namespace NetMvcExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
